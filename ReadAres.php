9ebe7e428218c1386351f032ea814846ef10c81f
Martin Dobry <32479112+dobraczek@users.noreply.github.com>
2019-04-08 08:10:12 +0200

    Update ReadAres.php

--- ReadAres.php
+++ ReadAres.php
@@ -11,8 +11,8 @@ namespace ReadAres;
 
 class Read {
 
-    public $ic;
-    public $link;
+	public $ic;
+	public $link;
 	
 	public function __construct($ic = null) {
 		$this->ic = $ic;
@@ -20,52 +20,52 @@ class Read {
 	}
 	
 	private function file_get_contents_curl($url) {
-	    $ch = curl_init();
-	    curl_setopt($ch, CURLOPT_HEADER, 0);
-	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	    curl_setopt($ch, CURLOPT_URL, $url);
-	    $data = curl_exec($ch);
-	    curl_close($ch);
-	    return $data;
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_URL, $url);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		return $data;
 	}
 	
 	public function testIC() {
 	    
-	    $in = $this->file_get_contents_curl($this->link."?ico=".$this->ic."&jazyk=cz&xml=1");
+		$in = $this->file_get_contents_curl($this->link."?ico=".$this->ic."&jazyk=cz&xml=1");
 	    
-	    preg_match_all('/<D:ET>(.*?)</s', $in, $error);
+		preg_match_all('/<D:ET>(.*?)</s', $in, $error);
 	    
-	    if($error[1][0]) {
+		if($error[1][0]) {
 	        
-	        $output = array('error' => $error[1][0]);
+			$output = array('error' => $error[1][0]);
 	    
-	    } else {
+		} else {
 	        
-    	    $tag = array(
-    	        'ICO' => 'IČ',
-    	        'OF' => 'Firma',
-    	        'N' => 'Město',
-    	        'NU' => 'Ulice',
-    	        'CD' => 'ČP',
-    	        'PSC' => 'PSČ',
-    	        'NPF' => 'Právní forma',
-    	        'Nazev_NACE' => 'ekonomických činností',
-    	        'KPP' => 'velikostní kat. dle počtu zam.',
-    	        'Zuj_kod_orig' => 'ZÚJ kód',
-    	        'NZUJ' => 'ZÚJ'
-    	    );
+			$tag = array(
+				'ICO' => 'IČ',
+				'OF' => 'Firma',
+				'N' => 'Město',
+				'NU' => 'Ulice',
+				'CD' => 'ČP',
+				'PSC' => 'PSČ',
+				'NPF' => 'Právní forma',
+				'Nazev_NACE' => 'ekonomických činností',
+				'KPP' => 'velikostní kat. dle počtu zam.',
+				'Zuj_kod_orig' => 'ZÚJ kód',
+				'NZUJ' => 'ZÚJ'
+			);
     	    
-    	    foreach ($tag as $key => $value) {
-    	        preg_match_all('/<D:'.$key.'>(.*?)</s', $in, $out);
-    	        $output[] = array(
-                    'name' => $value,
-    	            'value' => $out[1][0]
-    	        );
-    	    }
+			foreach ($tag as $key => $value) {
+				preg_match_all('/<D:'.$key.'>(.*?)</s', $in, $out);
+				$output[] = array(
+					'name' => $value,
+					'value' => $out[1][0]
+				);
+			}
     	    
-	    }
+		}
 	    
-	    return json_encode($output);
+		return json_encode($output);
 	}
 	
 }
