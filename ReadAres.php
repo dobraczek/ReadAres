4f919f4e79421bc9e31ab2b922fcba527683160b
Martin Dobry <32479112+dobraczek@users.noreply.github.com>
2019-04-08 07:56:14 +0200

    Add files via upload

---
+++ ReadAres.php
@@ -0,0 +1,71 @@
+<?php
+
+/**
+ * Read Ares
+ * @author Martin Dobry
+ * @link http://webscript.cz
+ * @version 1.0
+ */
+
+namespace ReadAres;
+
+class Read {
+
+    public $ic;
+    public $link;
+	
+	public function __construct($ic = null) {
+		$this->ic = $ic;
+		$this->link = 'https://wwwinfo.mfcr.cz/cgi-bin/ares/darv_res.cgi';
+	}
+	
+	private function file_get_contents_curl($url) {
+	    $ch = curl_init();
+	    curl_setopt($ch, CURLOPT_HEADER, 0);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	    curl_setopt($ch, CURLOPT_URL, $url);
+	    $data = curl_exec($ch);
+	    curl_close($ch);
+	    return $data;
+	}
+	
+	public function testIC() {
+	    
+	    $in = $this->file_get_contents_curl($this->link."?ico=".$this->ic."&jazyk=cz&xml=1");
+	    
+	    preg_match_all('/<D:ET>(.*?)</s', $in, $error);
+	    
+	    if($error[1][0]) {
+	        
+	        $output = array('error' => $error[1][0]);
+	    
+	    } else {
+	        
+    	    $tag = array(
+    	        'ICO' => 'IČ',
+    	        'OF' => 'Firma',
+    	        'N' => 'Město',
+    	        'NU' => 'Ulice',
+    	        'CD' => 'ČP',
+    	        'PSC' => 'PSČ',
+    	        'NPF' => 'Právní forma',
+    	        'Nazev_NACE' => 'ekonomických činností',
+    	        'KPP' => 'velikostní kat. dle počtu zam.',
+    	        'Zuj_kod_orig' => 'ZÚJ kód',
+    	        'NZUJ' => 'ZÚJ'
+    	    );
+    	    
+    	    foreach ($tag as $key => $value) {
+    	        preg_match_all('/<D:'.$key.'>(.*?)</s', $in, $out);
+    	        $output[] = array(
+                    'name' => $value,
+    	            'value' => $out[1][0]
+    	        );
+    	    }
+    	    
+	    }
+	    
+	    return json_encode($output);
+	}
+	
+}
---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<!--[if IE 8]> <html lang="en" class="ie8 no-js"> <![endif]-->
+<!--[if IE 9]> <html lang="en" class="ie9 no-js"> <![endif]-->
+<!--[if !IE]><!-->
+<html lang="en">
+<!--<![endif]-->
+
+<head>
+	<meta charset="utf-8">
+	<title>Read Ares</title>
+	
+	<meta content="" name="keywords">
+	<meta content="" name="author">
+	
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css"/>
+</head>
+<body>
+
+<h1>Read Ares</h1>
+
+<?php
+/**
+ * Read Ares
+ * @author Martin Dobry
+ * @link http://webscript.cz
+ * @version 1.0
+ */
+
+include "ReadAres.php";
+$Ares = new ReadAres\Read($_GET['ic']);
+$json = $Ares->testIC();
+
+$array = json_decode($json, 1);
+
+if($array[2])
+    foreach ($array as $data)
+        echo '<strong>'.$data['name'].':</strong> '.$data['value'].'<br />';
+else
+    echo '<strong>Nastala chyba!</strong><br />'.$array['error']
+
+?>
+
+</body>
+</html>
